c016330e4ec7ca3c9a650dbceef5ccfe3cf914e1
Makoto Yamashita <makochan12.9@gmail.com>
2020-08-08 16:50:24 +0900

    修正

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-<?php require_once($_SERVER["DOCUMENT_ROOT"] . "/php/components/headCommon.php"); ?>
+<?php require_once($_SERVER["DOCUMENT_ROOT"] . "/components/headCommon.php"); ?>
+<title>タイトル</title>
 </head>
 <body>
-<?php require_once($_SERVER["DOCUMENT_ROOT"] . "/php/components/footer.php"); ?>
-<?php require_once($_SERVER["DOCUMENT_ROOT"] . "/php/components/footCommon.php"); ?>
+<!-- 内容 -->
+<?php require_once($_SERVER["DOCUMENT_ROOT"] . "/components/footer.php"); ?>
+<?php require_once($_SERVER["DOCUMENT_ROOT"] . "/components/footCommon.php"); ?>
 </body>
 </html>
--- テンプレート.php
+++ テンプレート.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
-<?php require_once($_SERVER["DOCUMENT_ROOT"] . "/php/components/headCommon.php"); ?>
+<?php require_once($_SERVER["DOCUMENT_ROOT"] . "/components/headCommon.php"); ?>
 <title>タイトル</title>
 </head>
 <body>
-<?php require_once($_SERVER["DOCUMENT_ROOT"] . "/php/components/header.php"); ?>
-<?php require_once($_SERVER["DOCUMENT_ROOT"] . "/php/components/footer.php"); ?>
-<?php require_once($_SERVER["DOCUMENT_ROOT"] . "/php/components/footCommon.php"); ?>
+<?php require_once($_SERVER["DOCUMENT_ROOT"] . "/components/header.php"); ?>
+<!-- 内容 -->
+<?php require_once($_SERVER["DOCUMENT_ROOT"] . "/components/footer.php"); ?>
+<?php require_once($_SERVER["DOCUMENT_ROOT"] . "/components/footCommon.php"); ?>
 </body>
 </html>
