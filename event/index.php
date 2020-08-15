55d7658edc86b99a6123720d0a458801c7f2d17a
soliste16 <loup.solitaire1192@gmail.com>
2020-08-15 19:56:14 +0900

    ただの枠組み

---
+++ event/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang = "ja">
+<head>
+<meta charset = "utf-8">
+<title>参加団体ページ</title>
+<meta name = "keywords" content = "">
+<meta name = "description" content = "">
+</head>
+
+<body>
+
+  <header></header>
+
+  <!-- 目次 -->
+  <nav></nav>
+
+  <main>
+    <article></article>
+    <section class = "group-desc">
+    </section>
+  </main>
+
+  <!-- おすすめ企画 -->
+  <aside></aside>
+
+  <footer></footer>
+
+</body>
+
+</html>
