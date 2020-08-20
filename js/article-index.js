be610f471148952b0502ef22963f05f136111fa0
Makoto Yamashita <makochan12.9@gmail.com>
2020-08-20 10:24:35 +0900

    色が変わるわよ～

--- js/article-index.js
+++ js/article-index.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', function () {
   // .entry-content配下のh2、h3要素を全て取得する
   var matches = document.querySelectorAll('article h3, article h4');
 
+  h4Count = 0;
   // 取得した見出しタグ要素の数だけ以下の操作を繰り返す
   matches.forEach(function (value, i) {
     // 見出しタグ要素のidを取得し空の場合は内容をidにする
@@ -41,16 +42,29 @@ document.addEventListener('DOMContentLoaded', function () {
       // 追加する<ul><li><a>タイトル</a></li></ul>を準備する
       a.innerHTML = value.textContent;
       a.href = '#' + value.id;
+      // 目次の色変える
+      a.setAttribute("correnspondBox", ("articleBox" + String(h4Count)))
+
       li.appendChild(a)
       ul.appendChild(li);
 
       // 最後の<li>の中に要素を追加する
       lastLi.appendChild(ul);
+      h4Count++;
     }
   });
 
   // 最後に画面にレンダリング
   contentsList.appendChild(div);
+
+  // 目次の色変える
+  let matches2 = document.querySelectorAll('.box');
+  matchCount = 0;
+  matches2.forEach(function (value, i) {
+    var id2 = "articleBox" + String(matchCount);
+    value.id = id2;
+    matchCount++;
+  });
 });
 
 var $window = $(window), //ウィンドウを指定
@@ -65,6 +79,49 @@ $window.on("scroll", function () {
     $articleIndex.removeClass("fixed");
   }
 });
+
+// 今回の交差を監視する要素
+const boxes = document.querySelectorAll(".box");
+
+const options = {
+  root: null, // 今回はビューポートをルート要素とする
+  rootMargin: "-50% 0px", // ビューポートの中心を判定基準にする
+  threshold: 0 // 閾値は0
+};
+const observer = new IntersectionObserver(doWhenIntersect, options);
+// それぞれのboxを監視する
+boxes.forEach(box => {
+  observer.observe(box);
+});
+
+/**
+ * 交差したときに呼び出す関数
+ * @param entries
+ */
+function doWhenIntersect(entries) {
+  // 交差検知をしたもののなかで、isIntersectingがtrueのDOMを色を変える関数に渡す
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      activateIndex(entry.target);
+    }
+  });
+}
+
+/**
+ * 目次の色を変える関数
+ * @param element
+ */
+function activateIndex(element) {
+  // すでにアクティブになっている目次を選択
+  const currentActiveIndex = document.querySelector("#articleIndex .inView");
+  // すでにアクティブになっているものが0個の時（=null）以外は、activeクラスを除去
+  if (currentActiveIndex !== null) {
+    currentActiveIndex.classList.remove("inView");
+  }
+  // 引数で渡されたDOMが飛び先のaタグを選択し、activeクラスを付与
+  const newActiveIndex = document.querySelector(`a[correnspondbox='${element.id}']`);
+  newActiveIndex.classList.add("inView");
+}
 
 $(function(){
   $('a[href^="#"]').click(function(){
