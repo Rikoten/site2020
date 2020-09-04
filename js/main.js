de97d17f2dc8326badea2fea8afdf2ccb8148420
Makoto Yamashita <makochan12.9@gmail.com>
2020-09-04 15:30:59 +0900

    まだ仮だけどタイプ選択

--- js/main.js
+++ js/main.js
@@ -1,12 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
-
-
-
   /* イベント一覧のホバーアニメーション */
   const eventList = document.getElementById('eventList');
   const eventTile = Array.from( eventList.getElementsByClassName('eventTile') );
   eventTile[0].classList.add("active");
-  console.log(eventTile);
   eventTile.forEach( function(elem) {
     elem.addEventListener("mouseover", function(event) {
       let eventTileActive = Array.from( eventList.getElementsByClassName('eventTile active') );
@@ -19,10 +15,48 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 
   /* 表示切替 */
-  /* 初期状態 */
+  const eventTypePage = Array.from( eventList.getElementsByClassName('eventTypePage') );
+  const eventTypeTab = document.getElementById('eventTypeTab');
+  eventTypeTabBtn = Array.from(eventTypeTab.getElementsByTagName("a"));
 
-  /* 変化 */
+  eventTypeTabBtn.forEach(function(elem) {
+    elem.addEventListener('click', function (e) {
+      event.preventDefault();
+
+      /* ボタンの動作 */
+      eventTypeTabBtn.forEach(function(elem2) {
+        elem2.classList.remove('active');
+      });
+      elem.classList.add('active');
+
+      /* タブの動作 */
+      href = elem.getAttribute('href').substr(1);
+      eventTypePage.forEach(function(elem2){
+        if (elem2.id == href) elem2.classList.add('active');
+        else elem2.classList.remove('active');
+      });
+    });
+  });
+
+  /*eventTypePage.forEach(function(elem) {
+
+  });*/
+
+
+
+  /*const paginationBtn = Array.from(document.getElementsByClassName('paginationBtn'))
+  paginationBtn.forEach(function(elem) {
+    buttons = elem.firstElementChild('a');
+
+    elem.on('click', 'a', function(event) {
+      event.preventDefault();
+      let $this = $(this);
+
+      if ($this.classList.has("active")) return;
+
+    });
+  });
   
 
-  const eventListPageArticle = Array.from( eventList.getElementsByClassName('eventTile') );
+  const eventListPageArticle = Array.from( eventList.getElementsByClassName('eventTile') );*/
 });
