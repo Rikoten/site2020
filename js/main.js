95ea33ca55bfbe5b773cae799a58a0fea149d5b1
Makoto Yamashita <makochan12.9@gmail.com>
2020-09-04 13:30:11 +0900

    簡潔にこれが楽かなあ

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
 
+
+
+  /* イベント一覧のホバーアニメーション */
+  const eventList = document.getElementById('eventList');
+  const eventTile = Array.from( eventList.getElementsByClassName('eventTile') );
+  console.log(eventTile);
+  eventTile.forEach( function(elem) {
+    elem.addEventListener("mouseover", function(event) {
+      let eventTileActive = Array.from( eventList.getElementsByClassName('active') );
+      eventTileActive[0].classList.remove("active");
+      elem.classList.add("active");
+    }, false);
+    /*elem.addEventListener("mouseout", function(event) {
+      elem.classList.remove("active");
+    }, false);*/
+  });
 });
