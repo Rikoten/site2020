93aa4925603b8f0d15b8900a98e93fcdc9981c45
Makoto Yamashita <makochan12.9@gmail.com>
2020-11-03 21:33:31 +0900

    resolve関連

--- js/main.js
+++ js/main.js
@@ -69,114 +69,137 @@ const languageEvent = () => {
   }
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-  function eventLoad() {
-    return new Promise (function (resolve, reject) {
-      /* 企画ロード */
-      let URLEventData = 'data/eventData.json';
-      let lang = "ja";
-      
-      if(storageAvailable('localStorage')) {
-        if(localStorage.getItem("lang")) lang = localStorage.getItem("lang");
-      }
-      if(lang == "en") URLEventData = "/data/eventData_en.json";
+const placeCommonParts = new Promise ((resolve, reject) => {
+  let xhr = new XMLHttpRequest(),
+      method = "GET",
+      url = "/common.html";
+  let box = document.getElementById("common-header"),
+      box2 = document.getElementById("bottom-nav");
 
-      const requestEventData = new XMLHttpRequest();
-      requestEventData.open('GET', URLEventData);
-
-      requestEventData.responseType = 'json';
-      requestEventData.send();
-
-      requestEventData.onload = function() {
-        const EventData = requestEventData.response;
-
-        // Fisher–Yates アルゴリズムでシャッフルする
-        const EventDataShuffled = EventData;
-        for(let i = EventDataShuffled.length - 1; i > 0; i--){
-          let rand = Math.floor(Math.random() * (i + 1));
-          [EventDataShuffled[i], EventDataShuffled[rand]] = [EventDataShuffled[rand], EventDataShuffled[i]];
-        }
-        
-        /* ヘッダー部分 */
-        const topViewEventBar = document.getElementById('topView-event-bar');
-        const topViewEventBarMb = document.getElementById('topView-event-bar-mb');
-        /* 企画一覧部分 */
-        const tilesOnePage = 7; //1ページあたりの企画数
-        let TargetContainer = {article: document.getElementById('eventArticle'), movie: document.getElementById('eventMovie'), live: document.getElementById('eventLive')};  //コンテナ
-        let showedtiles = {article: 0, movie: 0, live: 0}; //表示された企画数
-        let showedPages = {article: 0, movie: 0, live: 0}; //生成されたページ数
-
-        EventDataShuffled.forEach(function(elem) {
-          /* ヘッダー部分 */
-          let HeaderTile = document.createElement('a');
-          let eventURL = '/event/?id=' + String(elem["eventID"]);
-          HeaderTile.setAttribute('href', eventURL);
-          HeaderTileInner = '<div class = "topView-event-tile ' + elem["eventType"] + '"><div class = "topView-event-type-container"><div class = "topView-event-type ' + elem["eventType"] + '"></div></div><div class = "topView-event-time">' + elem["requiredTime"] + '</div><div class = "topView-event-title">' + elem["eventName"] + '</div></div>'
-          HeaderTile.innerHTML = HeaderTileInner;
-
-          HeaderTileMb = HeaderTile.cloneNode(true);
-
-          topViewEventBar.appendChild(HeaderTile);
-          topViewEventBarMb.appendChild(HeaderTileMb);
-
-          /* 企画一覧部分 */
-          let type = elem["eventType"]; //企画タイプ
-
-          let childrenTag = ""; //子ども向けの企画タグ
-          if (elem["age"] == "child") childrenTag = '<div class = "eventChild child"><div>子ども向け</div></div>';
-          if (elem["age"] == "student") childrenTag = '<div class = "eventChild student"><div>受験生向け</div></div>';
-          
-          //TargetContainer[type]
-
-          /* ページネーション管理 */
-          if (showedtiles[type] % tilesOnePage == 0) {//新しいページ最初の企画
-            showedPages[type]++; //ページ数
-
-            /* ページネーションボタンの追加 */
-            let newPaginationBtn = document.createElement('a');
-            newPaginationBtn.setAttribute('href', '#');
-            newPaginationBtn.innerText = showedPages[type];
-            if (showedPages[type] == 1) newPaginationBtn.classList.add('active');
-            TargetContainer[type].getElementsByClassName('pagination')[0].appendChild(newPaginationBtn); //新しいページボタンの追加
-
-            /* 企画一覧ページの追加 */
-            let newPagination = document.createElement('div');
-            newPagination.classList.add('eventListPage');
-            if (showedPages[type] == 1) newPagination.classList.add('active');
-            TargetContainer[type].insertBefore(newPagination, TargetContainer[type].lastElementChild);
-          }
-
-          showedtiles[type]++;
-          
-          /* もっと見るボタン、5個の区切りずつで出力（PC版の仕様との干渉のため1つのボタンにできなかった） */
-          if (showedtiles[type] % 5 == 1 && showedtiles[type] > 1) {
-            let moreBtn = document.createElement('a');
-            let href_moreBtn = "";
-            if (type == "article") href_moreBtn = "#eventArticle";
-            else if (type == "movie") href_moreBtn = "#eventMovie";
-            else if (type == "live") href_moreBtn = "#eventLive";
-            moreBtn.setAttribute('href', href_moreBtn);
-            moreBtn.classList.add('more');
-            moreBtn.innerText = "もっと見る";
-            TargetContainer[type].lastElementChild.previousElementSibling.appendChild(moreBtn);
-          }
-
-          /* タイル追加 */
-          let EventListTile = document.createElement('a');
-          EventListTile.setAttribute('href', eventURL);
-          let EventListTileInner = '<div class = "eventTile"><div class = "eventTitle">' + elem["eventName"] + '</div><div class = "eventTime">' + elem["requiredTime"] + '</div>' + childrenTag + '<div class = "eventDesc">' + elem["pamphDesc"] + '</div><div class = "eventView"><span>73</span></div><div class = "eventThumbsUp">3</div><div class = "eventGroupName">' + elem["groupName"] + '</div></div>';
-
-          EventListTile.innerHTML = EventListTileInner;
-
-          TargetContainer[type].lastElementChild.previousElementSibling.appendChild(EventListTile);
-        });
-        resolve(EventDataShuffled);
-      }
-    });
+  xhr.responseType = "document";
+  xhr.open(method, url, true);
+  xhr.onreadystatechange = () => {
+    if(xhr.readyState === 4 && xhr.status === 200) {
+      let restxt = xhr.responseXML;
+      let int = restxt.getElementsByTagName("header")[0],
+          int2 = restxt.getElementsByTagName("nav")[0];
+      box.outerHTML = int.outerHTML;
+      //box2.outerHTML = int2.outerHTML;
+    }
   }
 
+  xhr.send();
+  resolve();
+});
+
+const eventLoad = placeCommonParts.then(() => {
+  return new Promise (function (resolve, reject) {
+    /* 企画ロード */
+    let URLEventData = 'data/eventData.json';
+    let lang = "ja";
+    
+    if(storageAvailable('localStorage')) {
+      if(localStorage.getItem("lang")) lang = localStorage.getItem("lang");
+    }
+    if(lang == "en") URLEventData = "/data/eventData_en.json";
+
+    const requestEventData = new XMLHttpRequest();
+    requestEventData.open('GET', URLEventData);
+
+    requestEventData.responseType = 'json';
+    requestEventData.send();
+
+    requestEventData.onload = function() {
+      const EventData = requestEventData.response;
+
+      // Fisher–Yates アルゴリズムでシャッフルする
+      const EventDataShuffled = EventData;
+      for(let i = EventDataShuffled.length - 1; i > 0; i--){
+        let rand = Math.floor(Math.random() * (i + 1));
+        [EventDataShuffled[i], EventDataShuffled[rand]] = [EventDataShuffled[rand], EventDataShuffled[i]];
+      }
+      
+      /* ヘッダー部分 */
+      const topViewEventBar = document.getElementById('topView-event-bar');
+      const topViewEventBarMb = document.getElementById('topView-event-bar-mb');
+      /* 企画一覧部分 */
+      const tilesOnePage = 7; //1ページあたりの企画数
+      let TargetContainer = {article: document.getElementById('eventArticle'), movie: document.getElementById('eventMovie'), live: document.getElementById('eventLive')};  //コンテナ
+      let showedtiles = {article: 0, movie: 0, live: 0}; //表示された企画数
+      let showedPages = {article: 0, movie: 0, live: 0}; //生成されたページ数
+
+      EventDataShuffled.forEach(function(elem) {
+        /* ヘッダー部分 */
+        let HeaderTile = document.createElement('a');
+        let eventURL = '/event/?id=' + String(elem["eventID"]);
+        HeaderTile.setAttribute('href', eventURL);
+        HeaderTileInner = '<div class = "topView-event-tile ' + elem["eventType"] + '"><div class = "topView-event-type-container"><div class = "topView-event-type ' + elem["eventType"] + '"></div></div><div class = "topView-event-time">' + elem["requiredTime"] + '</div><div class = "topView-event-title">' + elem["eventName"] + '</div></div>'
+        HeaderTile.innerHTML = HeaderTileInner;
+
+        HeaderTileMb = HeaderTile.cloneNode(true);
+
+        topViewEventBar.appendChild(HeaderTile);
+        topViewEventBarMb.appendChild(HeaderTileMb);
+
+        /* 企画一覧部分 */
+        let type = elem["eventType"]; //企画タイプ
+
+        let childrenTag = ""; //子ども向けの企画タグ
+        if (elem["age"] == "child") childrenTag = '<div class = "eventChild child"><div>子ども向け</div></div>';
+        if (elem["age"] == "student") childrenTag = '<div class = "eventChild student"><div>受験生向け</div></div>';
+        
+        //TargetContainer[type]
+
+        /* ページネーション管理 */
+        if (showedtiles[type] % tilesOnePage == 0) {//新しいページ最初の企画
+          showedPages[type]++; //ページ数
+
+          /* ページネーションボタンの追加 */
+          let newPaginationBtn = document.createElement('a');
+          newPaginationBtn.setAttribute('href', '#');
+          newPaginationBtn.innerText = showedPages[type];
+          if (showedPages[type] == 1) newPaginationBtn.classList.add('active');
+          TargetContainer[type].getElementsByClassName('pagination')[0].appendChild(newPaginationBtn); //新しいページボタンの追加
+
+          /* 企画一覧ページの追加 */
+          let newPagination = document.createElement('div');
+          newPagination.classList.add('eventListPage');
+          if (showedPages[type] == 1) newPagination.classList.add('active');
+          TargetContainer[type].insertBefore(newPagination, TargetContainer[type].lastElementChild);
+        }
+
+        showedtiles[type]++;
+        
+        /* もっと見るボタン、5個の区切りずつで出力（PC版の仕様との干渉のため1つのボタンにできなかった） */
+        if (showedtiles[type] % 5 == 1 && showedtiles[type] > 1) {
+          let moreBtn = document.createElement('a');
+          let href_moreBtn = "";
+          if (type == "article") href_moreBtn = "#eventArticle";
+          else if (type == "movie") href_moreBtn = "#eventMovie";
+          else if (type == "live") href_moreBtn = "#eventLive";
+          moreBtn.setAttribute('href', href_moreBtn);
+          moreBtn.classList.add('more');
+          moreBtn.innerText = "もっと見る";
+          TargetContainer[type].lastElementChild.previousElementSibling.appendChild(moreBtn);
+        }
+
+        /* タイル追加 */
+        let EventListTile = document.createElement('a');
+        EventListTile.setAttribute('href', eventURL);
+        let EventListTileInner = '<div class = "eventTile"><div class = "eventTitle">' + elem["eventName"] + '</div><div class = "eventTime">' + elem["requiredTime"] + '</div>' + childrenTag + '<div class = "eventDesc">' + elem["pamphDesc"] + '</div><div class = "eventView"><span>73</span></div><div class = "eventThumbsUp">3</div><div class = "eventGroupName">' + elem["groupName"] + '</div></div>';
+
+        EventListTile.innerHTML = EventListTileInner;
+
+        TargetContainer[type].lastElementChild.previousElementSibling.appendChild(EventListTile);
+      });
+      resolve(EventDataShuffled);
+    }
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
   /* ここからは企画データ表示後の対応が必要！ */
-  eventLoad().then(function(EventDataShuffled) {
+  eventLoad.then((EventDataShuffled) => {
     languageEvent();
     /* イベント一覧のホバーアニメーション */
     const eventList = document.getElementById('eventList');
@@ -370,28 +393,5 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   });
 
-  /* 指定位置にcommon.htmlからheader, footerを読み込む */
-
-  let xhr = new XMLHttpRequest(),
-      method = "GET",
-      url = "/common.html";
-  let box = document.getElementById("common-header"),
-    box2 = document.getElementById("bottom-nav"),
-    box3 = document.getElementsByTagName("footer")[0];
-
-  xhr.responseType = "document";
-  xhr.open(method, url, true);
-  xhr.onreadystatechange = () => {
-    if(xhr.readyState === 4 && xhr.status === 200) {
-      let restxt = xhr.responseXML;
-      let int = restxt.getElementsByTagName("header")[0],
-        int2 = restxt.getElementsByTagName("nav")[0],
-        int3 = restxt.getElementsByTagName("footer")[0];
-      box.outerHTML = int.outerHTML;
-      box2.outerHTML = int2.outerHTML;
-      box3.outerHTML = int3.outerHTML;
-    }
-  };
-
-  xhr.send();
+  
 });
