fd6e2d5763f5cef606bab99c8ca5a0ff9c537dbd
soliste16 <loup.solitaire1192@gmail.com>
2020-11-04 21:00:25 +0900

    ヘッダーのガワ

---
+++ js/common.js
@@ -0,0 +1,16 @@
+window.onload = () => {
+  const $fieldset = document.querySelector("#sticky-header fieldset");
+  let startPos = 0;
+
+  window.addEventListener("scroll", () => {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    let currentPos = scrollTop;
+
+    if (currentPos > startPos) {
+      $fieldset.classList.add("fold");
+    } else {
+      $fieldset.classList.remove("fold");
+    }
+    startPos = currentPos;
+  });
+}
