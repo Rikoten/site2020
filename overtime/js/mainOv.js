450c86cb3bed1d22669da5ff387449e059a6d6ce
comame <dev@comame.xyz>
2020-11-08 08:07:54 +0900

    文面の変更

--- overtime/js/mainOv.js
+++ overtime/js/mainOv.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
   const now = Math.floor((new Date()).getTime() / 1000);
   const threshold = 1604833200;
   if (now >= threshold) {
-    document.getElementById('desc1').innerText = "第67回理工展は無事終了いたしました。";
-    document.getElementById('desc2').innerText = "ご来場ありがとうございました！";
+    document.getElementById('desc1').innerHTML = "第 67 回理工展は終了いたしました。ご来場ありがとうございました。<br>理工展パンフレットアプリ及びバーチャル理工展アプリは引き続きお楽しみいただけます。";
+    document.getElementById('desc2').remove()
   }
 });
